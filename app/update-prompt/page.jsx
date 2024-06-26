1f72f443ea4c60e0ca5540554f6e6fc8897c5c31
Brian Joe Antoni <brianjoeantoni@gmail.com>
2024-06-26 19:29:50 +0700

    fix suspense boundary at page "/update-prompt"

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,8 +1,7 @@
 "use client";
-// as soon as we import a hook, we turn it into a 'use client' page
-import { useState, useEffect } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
 
+import { useState, useEffect, Suspense } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
 import Form from "@components/Form";
 
 const EditPrompt = () => {
@@ -17,36 +16,35 @@ const EditPrompt = () => {
     const getPromptDetails = async () => {
       const response = await fetch(`/api/prompt/${promptId}`);
       const data = await response.json();
-
       setPost({ prompt: data.prompt, tag: data.tag });
     };
     if (promptId) getPromptDetails();
   }, [promptId]);
 
-    const updatePrompt = async (e) => {
-      e.preventDefault(); // prevents auto reload
-      setSubmitting(true);
+  const updatePrompt = async (e) => {
+    e.preventDefault();
+    setSubmitting(true);
 
-      if(!promptId) return alert("Prompt ID not found");
+    if (!promptId) return alert("Prompt ID not found");
 
-      try {
-        const response = await fetch(`/api/prompt/${promptId}`, {
-          method: "PATCH",
-          body: JSON.stringify({
-            prompt: post.prompt,
-            tag: post.tag,
-          }),
-        });
+    try {
+      const response = await fetch(`/api/prompt/${promptId}`, {
+        method: "PATCH",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          tag: post.tag,
+        }),
+      });
 
-        if (response.ok) {
-          router.push("/");
-        }
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setSubmitting(false);
+      if (response.ok) {
+        router.push("/");
       }
-    };
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
 
   return (
     <Form
@@ -59,4 +57,10 @@ const EditPrompt = () => {
   );
 };
 
-export default EditPrompt;
+const EditPromptPage = () => (
+  <Suspense fallback={<div>Loading...</div>}>
+    <EditPrompt />
+  </Suspense>
+);
+
+export default EditPromptPage;
